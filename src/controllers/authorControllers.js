43b3631772b3e65e7da1b426c775043d53dc0ea5
pravatkumargupta8249729269 <pravatkumargupta96@gmail.com>
2022-04-18 18:10:47 +0530

    14th april Assignment

---
+++ src/controllers/authorControllers.js
@@ -0,0 +1,7 @@
+const authorModel= require("../models/authorModel")
+const creatAuthor= async function (req,res) {
+    let author =req.body
+    let authorCreated = await authorModel.create(author)
+    res.send({data: authorCreated})
+}
+module.exports.creatAuthor=creatAuthor
--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://urajrishu:aUHDB96UyJaq9SB@cluster0.1wief.mongodb.net/pravatkumargupta-db", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
 app.use('/', route);
 
-app.listen(process.env.PORT || 4000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 4000))
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
